2a546546e6ef523f8e60bfc35e12302be02ad505
ArraisLucas <lucas.darrais@gmail.com>
2021-08-23 18:54:05 -0300

    criação do repositorio e do index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//incluindo os dados
+include 'dados/dados.php';
+//inclusao do header
+include 'pages/header.php';
+//inclusao do arry $pagina= 'home';
+include "./functions/functions.php";
+
+//inclusao do footer
+include "pages/footer.php";
+?>
