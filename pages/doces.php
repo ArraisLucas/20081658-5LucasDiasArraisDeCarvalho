e3e28a3d2222e254031af490f924667763cc5282
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 19:57:40 -0300

    criação do arq. doces.php

---
+++ pages/doces.php
@@ -0,0 +1,20 @@
+<h2>Doces</h2>
+
+
+
+
+<div class="main">
+    <aside class="texto">
+    <?php echo $produtos["doces"]["descricao"]?>    
+    </aside>
+
+    <div>
+     <img class="img-prod" src="./imagens/doce.jpg"  alt="imagem_de_doce">
+     <div class="botao-valor">R$<?php echo $produtos["doces"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+
+
+</body>
+</html>
