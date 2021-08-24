9b19ef8d38a6b8a62411ab7410cdb6f9fc857bfa
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 19:56:09 -0300

    criação do arq. contato.php

---
+++ pages/contato.php
@@ -0,0 +1,25 @@
+<div class="container-contatos">
+    <h2>Contatos</h2>
+    <div class="cont">
+
+    <aside>
+        <p> Não encontrou aquele seu Potão Mágico  favorito ? Entre em contato com a gente, ficaremos felizes em fazer-lo especialmente para voce !!! </p>
+    </aside>
+
+    <div class="contatos">
+    <label for="">Seu Nome:</label>
+    <input type="text" name="nome" placeholder="Nome" id="nome">
+    
+    <label for="email">Digite seu E-mail:</label>
+    <input type="email" placeholder="E-mail" id="email" name="email" required>
+
+    <label for="">Deixe sua mensagem:</label>
+    <textarea name="" id="" cols="30" rows="8"></textarea>
+    <br>
+    <buton type="button" value="Enviar" onclick="evento()">Enviar</buton>
+ <script>
+   
+ </script>
+    </div>
+    </div>
+</div>
