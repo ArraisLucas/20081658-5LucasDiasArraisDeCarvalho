a9ac834f124be82560e9900794229579b69c6423
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 19:54:51 -0300

    criaçao do arq. compota.php

---
+++ pages/compota.php
@@ -0,0 +1,17 @@
+<h2>Compotas</h2>
+
+<div class="main">
+    <aside class="texto">
+        <?php echo $produtos["compota"]["descricao"]?>
+    </aside>
+
+    <div>
+        <img class="img-prod"  src="./imagens/compota.jpg"  alt="imagem_de_compota">
+        <div class="botao-valor">R$<?php echo $produtos["compota"]["preco"]?></div>
+        <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+
+
+
+</html>
