49d28a04feb908760ff4d01c385e1546bf6a822c
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 20:03:26 -0300

    criação do arq. home.php

---
+++ pages/home.php
@@ -0,0 +1,40 @@
+<h2>Nossos Produtos</h2>
+
+<div class="produtos">
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/compota.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Compota</h5>
+    <p class="card-text">Um pote de frutas picadas em conserva, feito com a mais pura Magia....
+</p>
+    <a href="./index.php?p=compota" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/geleia.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Geleias</h5>
+    <p class="card-text">Nadinha de sabores artificiais, receitas exclusivas, feitas com ingredientes frescos e naturais...</p>
+    <a href="./index.php?p=geleia"  class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/doce.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Doces</h5>
+    <p class="card-text">Doces em cubinhos , de diversos sabores, tudo natural...</p>
+    <a href="./index.php?p=doces" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/cookie.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Cookies</h5>
+    <p class="card-text">Cookies Naturais, com produtos organicos e nada de glúten</p>
+    <a href="./index.php?p=cookie" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+</div>
