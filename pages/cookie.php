55c9babe4220a465a6d7b12e12df8eee7467a24a
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 19:57:03 -0300

    criação da pagina cookie.php

---
+++ pages/cookie.php
@@ -0,0 +1,18 @@
+<h2>Cookies</h2>
+
+
+<div class="main">
+    <aside class="texto">
+    <?php echo $produtos["cookie"]["descricao"]?>    
+    </aside>
+
+    <div>
+     <img class="img-prod" src="./imagens/cookie.jpg"  alt="imagem_de_bolos">
+     <div class="botao-valor">R$<?php echo $produtos["cookie"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+
+
+</body>
+</html>
