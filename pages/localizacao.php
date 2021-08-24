32d98568337628b55ea6be73b9a8e3a65569bf29
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 20:04:00 -0300

    criação do arq. localização.php

---
+++ pages/localizacao.php
@@ -0,0 +1,9 @@
+<h2>Nossa Localização</h2>
+<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1409.2244324690528!2d-44.17561902429965!3d-2.543179486399818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7f690d437c373bd%3A0x48023cdb812e69f0!2sAv.%2013%2C%201%20-%20Maiob%C3%A3o%2C%20Pa%C3%A7o%20do%20Lumiar%20-%20MA%2C%2065130-000!5e0!3m2!1spt-BR!2sbr!4v1629826833053!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+
+
+  
+</body>
+</html>
