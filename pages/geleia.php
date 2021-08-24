ac93e13eab6ad47a6c5bdf4631ce1253ff42e15c
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 19:59:54 -0300

    criação do arq.  geleia.php

---
+++ pages/geleia.php
@@ -0,0 +1,17 @@
+<h2>Geleia</h2>
+
+
+
+<div class="main">
+    <aside class="texto">
+        <?php echo $produtos["geleia"]["descricao"]?>
+    </aside>
+
+    <div>
+    <img class="img-prod" src="./imagens/geleia.jpg"  alt="imagem_de_geleias">
+     <div class="botao-valor">R$<?php echo $produtos["geleia"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+</body>
+</html>
