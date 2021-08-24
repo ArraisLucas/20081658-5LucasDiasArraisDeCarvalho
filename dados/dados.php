d66692dba0719c8fbdd6cb170c01ab0c3c8f0c2b
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 16:10:17 -0300

    alteraçoes feitas no arq. dados.php

--- dados/dados.php
+++ dados/dados.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Potão Mágico</title>
     </head>
     <body>
      
@@ -14,23 +14,23 @@
 <?php
 $produtos = [
     "compota" =>[ 
-      "descricao"=>" ",
+      "descricao"=>" Um potão com frutas mágicas especiais para você .... ",
       'preco'=>16.99,
       
     ],
-    'cupcake'=>[
-      'descricao'=>" ",
+    'geleia'=>[
+      'descricao'=>"Um Potão com uma Geleia Mágica Especial, que você nao vai conseguir parar de comer ",
       'preco'=> 7.99,
       
     ], 
-    'artesanal'=>[
-      'descricao'=>" ",
-      'preco'=> 59.99,
+    'doces'=>[
+      'descricao'=>" Doces Naturais Feitos com frutas frescas, coberto com chocolate vegano,  uma delicia... ",
+      'preco'=> 29.99,
       
     ],
   
-    'brownie'=>[
-      'descricao'=>"",
+    'cookie'=>[
+      'descricao'=>" Nossos cookies são naturais, sem glúten ou lactose e com produtos de primeiríssima qualidade, garantindo sabor incomparável. E saudável!",
       'preco'=> 9.99,
       
     ],
