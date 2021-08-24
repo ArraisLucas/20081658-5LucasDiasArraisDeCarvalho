05811818ac71881e70566bacaed77c52ebe695d6
ArraisLucas <lucas.darrais@gmail.com>
2021-08-24 20:04:20 -0300

    criação do arq. quemsomos.php

---
+++ pages/quemsomos.php
@@ -0,0 +1,15 @@
+<h2>Quem Somos</h2>
+<div class="main">
+
+
+
+<p>A Potão Mágico  iniciou suas atividades em 2021 e aos poucos foi se tornando conhecida pelo seus Produtos Naturais  e pelos saborosos cookies, doces e Compotas produzidas com ingredientes frescos.
+
+Nossa equipe trabalha com afinco na qualidade, procurando deixar tudo com muito sabor e perfeição. O profissionalismo e a dedicação são constantes em nossa empresa, sendo que buscamos sempre o aperfeiçoamento de técnicas, novas receitas.
+Usando um toque de Magica, deixando você hipnotizado a cada mordida... </p>
+
+
+<img src="./imagens/logotipo.png" alt="logo-magico" style="width: 340px;">
+
+</div>
+
